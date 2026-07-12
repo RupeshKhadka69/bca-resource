09bbfcc5c6e86cba368ccfc34474dea9bb055087
Rupesh Khadka <khadkarupesh456@gmail.com>
2026-07-12 14:39:20 +0545

    fix() typo in env

--- src/config/env.ts
+++ src/config/env.ts
@@ -7,6 +7,7 @@ const envSchema = z.object({
   JWT_SECRET: z.string().min(1),
   JWT_EXPIRES_IN: z.string().min(1),
   CLOUDINARY_CLOUD_NAME: z.string().min(1),
+  CLOUDINARY_URL: z.string().min(1),
   CLOUDINARY_API_KEY: z.string().min(1),
   CLOUDINARY_API_SECRET: z.string().min(1),
   ADMIN_USERNAME: z.string().min(1).optional(),
